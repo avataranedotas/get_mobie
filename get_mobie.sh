d62306edd74bc4cfd945cdd94c3712115571c98c
Alexandre Moleiro <avataranedotas@yahoo.com>
2021-12-30 11:23:00 +0000

    Update get_mobie.sh

--- get_mobie.sh
+++ get_mobie.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-AGORA=`date -d "today 00:00" +"%Y-%m-%dT%TZ"`
-
+AGORA=`date +"%Y%m%dT%H%M%S"`
 
 echo Hoje  : $AGORA
+
+#ir buscar o ficheiro
+wget -O $AGORA.json "https://ocpi.mobinteli.com/2.2/locations"
+
