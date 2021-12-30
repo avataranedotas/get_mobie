476747950f78fb79d2a7f05165d80d0c0d9f5e24
Alexandre Moleiro <avataranedotas@yahoo.com>
2021-12-30 11:51:52 +0000

    testes

---
+++ compare.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+"""
+explanation of json-module
+json.load(file_obj) : file_obj --> dict
+json.loads(json_str) : json_str --> dict
+json.dumps(dict) : dict --> json_str
+json.dump(dict, file_obj) : write variable-of-dict to JSON file
+If json_file contains 2 or more objects, "json.load(file_obj)"　will be to an error.
+As JSON-file contains multiple objects, "json.load()" cannot extract 2 or more objects from JSON file at once.
+"""
+
+with open("LATEST.json", mode="r", encoding="utf-8") as f:
+    dict = json.load(f)
+    for key in dict:
+        print(dict[key])
