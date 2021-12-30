f3e1346ca784f154c12a47c6196d11a1036fd74c
Alexandre Moleiro <avataranedotas@yahoo.com>
2021-12-30 12:02:36 +0000

    teste5

--- compare.py
+++ compare.py
@@ -2,18 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import json
-
-"""
-explanation of json-module
-json.load(file_obj) : file_obj --> dict
-json.loads(json_str) : json_str --> dict
-json.dumps(dict) : dict --> json_str
-json.dump(dict, file_obj) : write variable-of-dict to JSON file
-If json_file contains 2 or more objects, "json.load(file_obj)"　will be to an error.
-As JSON-file contains multiple objects, "json.load()" cannot extract 2 or more objects from JSON file at once.
-"""
+import sys
 
 #with open("./LATEST.json", mode="r", encoding="utf-8") as f:
 #    dict = json.load(f)
 #    for key in dict:
 #        print(dict[key])
+
+
+json_filename="./LATEST.json"
+print json_filename
+
+try:
+    json_file=open(json_filename)
+    json_data=json.load(json_file)
+except IOError:
+    print "Error: File does not appear to exist."
+finally:
+    json_file.close()
+
+my_dict={}
+
+for item in json_data['result']:
+    #print item['templateid'], ": ",item['name']
+    my_dict.update({ item['templateid']: item['name']})
+
+for key in sorted(my_dict.iterkeys()):
+    print "%s: %s" % (key, my_dict[key])
