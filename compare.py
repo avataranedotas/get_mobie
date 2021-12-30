d3b564d6905a76a6766b59cac9fca1e7f1bce3cc
Alexandre Moleiro <avataranedotas@yahoo.com>
2021-12-30 12:37:20 +0000

    teste15

--- compare.py
+++ compare.py
@@ -5,17 +5,17 @@ import json
 import sys
 
 # Opening JSON file
-with open('./teste.json') as json_file:
+with open('./LATEST.json') as json_file:
     data = json.load(json_file)
  
-    # mostrar o registo n
-    print(data[0])
+    # debug mostrar o registo n
+    # print(data[0])
      
     # for printing the key-value pair of
     # nested dictionary for loop can be used
-    print("\nPrinting nested dictionary as a key-value pair\n")
+    print("\nLista completa de id's\n")
     for i in data:
         print(i["id"])
-        print()
+    
 
 
