f19d3b5efb011e38032b27c092bd74428384de47
Alexandre Moleiro <avataranedotas@yahoo.com>
2023-10-06 22:34:09 +0100

    Add files via upload

---
+++ get_mobie_20231006.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AGORA=`date +"%Y%m%dT%H%M%S"`
+
+echo Hoje  : $AGORA
+
+#renomear lastest para previous
+rm PREVIOUS.json
+mv LATEST.json PREVIOUS.json
+
+#ir buscar o ficheiro
+#wget -O LATEST.json "https://ocpi.mobinteli.com/2.2/locations" --no-check-certificate
+
