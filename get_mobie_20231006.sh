da499d8a225d01089526c9191e2ec15c9cebe6b3
Alexandre Moleiro <avataranedotas@yahoo.com>
2023-10-08 15:08:56 +0100

    Update get_mobie_20231006.sh

--- get_mobie_20231006.sh
+++ get_mobie_20231006.sh
@@ -5,8 +5,8 @@ AGORA=`date +"%Y%m%dT%H%M%S"`
 echo Hoje  : $AGORA
 
 #renomear lastest para previous
-#rm PREVIOUS.json
-#mv LATEST.json PREVIOUS.json
+rm PREVIOUS.json
+mv LATEST.json PREVIOUS.json
 
 #ir buscar o ficheiro API 2023
 
